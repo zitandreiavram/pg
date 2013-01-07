8b3fc512d65a9421bcf9e5803189b43c26e4c24d
Andrei <zit_andreiavram@yahoo.com>
2013-01-07 11:38:39 +0200

    js

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ var app = {
         // note that this is an event handler so the scope is that of the event
         // so we need to call app.report(), and not this.report()
         app.report('deviceready');
+        app.register();
     },
     report: function(id) { 
         console.log("report:" + id);
@@ -16,5 +17,27 @@ var app = {
         document.querySelector('#' + id + ' .pending').className += ' hide';
         var completeElem = document.querySelector('#' + id + ' .complete');
         completeElem.className = completeElem.className.split('hide').join('');
+    },
+    register: function() {
+        var pushNotification = window.plugins.pushNotification;
+        pushNotification.registerDevice({alert:true, badge:true, sound:true}, function(status) {
+            app.myLog.value+=JSON.stringify(['registerDevice status: ', status])+"\n";
+            app.storeToken(status.deviceToken);
+        });
+    },
+    myLog: document.getElementById("log"),
+    storeToken: function(token) {
+        console.log("Token is " + token);
+        var xmlhttp=new XMLHttpRequest();
+        xmlhttp.open("POST","http://127.0.0.1:8888",true);
+        xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        xmlhttp.send("token="+token+"&message=pushnotificationtester");
+        xmlhttp.onreadystatechange=function() {
+            if (xmlhttp.readyState==4) {
+                //a response now exists in the responseTest property.
+                console.log("Registration response: " + xmlhttp.responseText);
+                app.myLog.value+="Registration server returned: " + xmlhttp.responseText;
+            }
+        }
     }
 };
